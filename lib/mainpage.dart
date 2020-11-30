a331fcd02748d97095d78a42a4b55dc374df1336
StingBugz <54566983+StingBugz@users.noreply.github.com>
2020-11-30 22:36:17 +0700

    commit ketiga

--- lib/mainpage.dart
+++ lib/mainpage.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 class MainPage extends StatefulWidget {
   @override
@@ -8,18 +7,65 @@ class MainPage extends StatefulWidget {
 
 class _MainPageState extends State<MainPage> {
   @override
-  void initState() {
-    super.initState();
-    SystemNavigator.pop();
-  }
-
-  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(
-          child: Text("hello world !!"),
+        appBar: AppBar(
+          title: Text("Covid app"),
+          backgroundColor: Colors.blue,
+        ),
+        body: Stack(
+          children: <Widget>[
+            //stack pertama
+            Align(
+              alignment: Alignment.topCenter,
+              child: Text("disini adalaah background"),
+            ),
+            //stack kedua
+            Align(
+              alignment: Alignment.center,
+              child: Text(
+                  "disini untuk card view informasi / tabbar view pertama"),
+            ),
+            //stack ketiga
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: DefaultTabController(
+                length: 3,
+                child: Container(
+                  height: 50,
+                  color: Colors.blueAccent,
+                  child: TabBar(
+                      indicatorColor: Colors.purple,
+                      labelPadding: EdgeInsets.all(10),
+                      tabs: [
+                        Tab(
+                          child: Image(
+                            width: 50,
+                            height: 50,
+                            image:
+                                AssetImage("assets/image/virus-molecule.png"),
+                          ),
+                        ),
+                        Tab(
+                          child: Image(
+                              width: 50,
+                              height: 50,
+                              image: AssetImage("assets/image/book.png")),
+                        ),
+                        Tab(
+                          child: Image(
+                            image: AssetImage("assets/image/first-aid-kit.png"),
+                            width: 50,
+                            height: 50,
+                          ),
+                        )
+                      ]),
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );
